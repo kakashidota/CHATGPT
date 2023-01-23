39af540137be5cea2a488a910a7f16d701f6ac40
Kakashidota <robin.kamo@live.se>
2023-01-23 03:34:40 +0100

    nice

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const PORT = process.env.PORT || 3000
+const https = require('https')
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/joke', (req, res) => {
+  https.get('https://api.chucknorris.io/jokes/random', (response) => {
+    response.on('data', (data) => {
+      console.log(data)
+      res.send(JSON.parse(data))
+    })
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
 });
 
 app.listen(PORT, () => {
